2a68213e2589cdee7991edb04ed2d6fcdbc7c4fd
al150422 <58019708+al150422@users.noreply.github.com>
2019-11-21 17:51:03 -0700

    Add files via upload

---
+++ Examen.r
@@ -0,0 +1,25 @@
+# 1.- 
+number <- c(1:8)
+number
+planets <-  c("Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune")
+planets[4]
+planets[6]
+
+#2.-
+matriz <- matrix(1:50, nrow=10,byrow=TRUE)
+matriz
+
+#3.- 
+food <- c("Breakfast", "Breakfast", "Breakfast", "Luch", "Luch", "Dinner", "Dinner","Dinner","Collation","Collation","Collation")
+food
+factorfood <- factor(food)
+factorfood
+
+#4.-
+Course <- c("Fundamentals of programnig", "Object-Oriented programing", "Databases","Technology innovation","Math")
+Mark <- c(10,8,4,6,10)
+Professor <- c("Alan Ponce","Abraham Lopez", "Vicente Garcia", "Lucero Zamora", "Francisco Lopez")
+ccc <- data.frame(Course, Mark, Professor)
+ccc
+
+#5.-
